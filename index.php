e61ee986b819181a87e925680b17e0257acd851d
kc652@njit.edu <kc652@njit.edu>
2024-01-24 19:12:09 -0500

    h

---
+++ index.php
@@ -0,0 +1,17 @@
+<h2>hello world</h2>
+
+<?php 
+echo "test: ";
+$secret = "password123"; // wont print out 
+echo $secret;
+$stack =[1,2,3];
+echo $stack[1]; // W3S php-looping. 
+?>
+<h2>new line</h2>
+<?php 
+echo "test: ";
+$secret = "password123"; // wont print out 
+echo $secret;
+$stack =[1,2,3];
+echo $stack[1]; // W3S php-looping. 
+?>
