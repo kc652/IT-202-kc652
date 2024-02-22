bb8589bbfca4c775ae364453a65929a68fb4facb
kc652@njit.edu <kc652@njit.edu>
2024-02-21 20:12:05 -0500

    h

--- M1/partials/nav.php
+++ M1/partials/nav.php
@@ -9,24 +9,42 @@ $localWorks = true; //some people have issues with localhost for the cookie para
 //if you're one of those people make this false
 
 //this is an extra condition added to "resolve" the localhost issue for the session cookie
-if (($localWorks && $domain == "localhost") || $domain != "localhost") {
-    session_set_cookie_params([
-        "lifetime" => 60 * 60,
-        "path" => "/Project",
-        //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
-        "domain" => $domain,
-        "secure" => true,
-        "httponly" => true,
-        "samesite" => "lax"
-    ]);
-}
+
 require_once(__DIR__ . "/../lib/functions.php");
 
 ?>
 <nav>
+    <style>
+        ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: #111;
+}
+</style>
     <ul>
         <?php if (is_logged_in()) : ?>
             <li><a href="home.php">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="logout.php">Logout</a></li>
         <?php endif; ?>
         <?php if (!is_logged_in()) : ?>
             <li><a href="login.php">Login</a></li>
