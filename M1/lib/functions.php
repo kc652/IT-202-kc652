a6f487c35b1805d669438377f3e01fa35fef508c
kc652@njit.edu <kc652@njit.edu>
2024-04-10 20:48:41 -0400

    flash

--- M1/lib/functions.php
+++ M1/lib/functions.php
@@ -16,24 +16,3 @@ require(__DIR__ . "/user_helpers.php");
 require(__DIR__. "/flash_messages.php");
 flash_messages.php
 <?php
-function flash($msg = "", $color = "info")
-{
-    $message = ["text" => $msg, "color" => $color];
-    if (isset($_SESSION['flash'])) {
-        array_push($_SESSION['flash'], $message);
-    } else {
-        $_SESSION['flash'] = array();
-        array_push($_SESSION['flash'], $message);
-    }
-}
-
-function getMessages()
-{
-    if (isset($_SESSION['flash'])) {
-        $flashes = $_SESSION['flash'];
-        $_SESSION['flash'] = array();
-        return $flashes;
-    }
-    return array();
-}
-?>
